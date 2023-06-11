4e056b6c58f14b8a975d1cbfb4710f0ef51cb823
pmarchionno <pmarchionno@gmail.com>
2023-06-11 16:07:19 -0300

    first commit

---
+++ src/main/java/com/project/basebackup/BasebackupApplication.java
@@ -0,0 +1,13 @@
+package com.project.basebackup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasebackupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasebackupApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/basebackup/BasebackupApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.basebackup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BasebackupApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
